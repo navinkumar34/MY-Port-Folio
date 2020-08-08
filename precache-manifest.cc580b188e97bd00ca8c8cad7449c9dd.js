1e080bfff0b4ffb3ca445dd4d968fba641243157
Navin Kumar <navinredu@gmail.com>
2020-08-08 18:44:04 +0530

    Updates

---
+++ precache-manifest.cc580b188e97bd00ca8c8cad7449c9dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc035003679836a50e4ab2d14ef99cc3",
+    "url": "/MY-Port-Folio/index.html"
+  },
+  {
+    "revision": "c29c99423bbe002d051c",
+    "url": "/MY-Port-Folio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4fcb68506f67704e9f08",
+    "url": "/MY-Port-Folio/static/css/main.3eab7fcf.chunk.css"
+  },
+  {
+    "revision": "c29c99423bbe002d051c",
+    "url": "/MY-Port-Folio/static/js/2.c535c3ff.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MY-Port-Folio/static/js/2.c535c3ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fcb68506f67704e9f08",
+    "url": "/MY-Port-Folio/static/js/main.9630186c.chunk.js"
+  },
+  {
+    "revision": "050b9b64ff56dab08147",
+    "url": "/MY-Port-Folio/static/js/runtime-main.45dfb0f3.js"
+  }
+]);
